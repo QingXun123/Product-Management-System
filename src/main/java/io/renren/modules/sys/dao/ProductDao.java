b0ff8e6740d18107f2c61d52621c29088915de51
QingXun123 <947338658@qq.com>
2022-10-23 21:51:21 +0800

    添加了产品信息功能

---
+++ src/main/java/io/renren/modules/sys/dao/ProductDao.java
@@ -0,0 +1,11 @@
+package io.renren.modules.sys.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.renren.modules.sys.entity.ProductEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductDao extends BaseMapper<ProductEntity> {
+
+}
---
+++ src/main/java/io/renren/modules/sys/service/impl/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package io.renren.modules.sys.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.renren.common.utils.PageUtils;
+import io.renren.common.utils.Query;
+import io.renren.modules.sys.dao.ProductDao;
+import io.renren.modules.sys.entity.ProductEntity;
+import io.renren.modules.sys.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service("productService")
+public class ProductServiceImpl extends ServiceImpl<ProductDao, ProductEntity> implements ProductService {
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<ProductEntity> page = this.page(
+                new Query<ProductEntity>().getPage(params),
+                new QueryWrapper<ProductEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+}
